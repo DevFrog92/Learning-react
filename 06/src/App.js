b5fb09e9440b190ff818af5607fbfab5d175bde6
DevFrog92 <yongsu1481@gmail.com>
2022-10-20 15:43:20 +0900

    ✨ Implement Form and FormContainer components

--- 06/src/App.js
+++ 06/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import MainPage from "./pages/MainPage";
 import "./assets/styles/main.css";
+import FormContainer from "./container/FormContainer";
 
 const App = () => {
-  return <MainPage />
+  return (
+    <>
+      <MainPage />
+      <hr/>
+      <FormContainer />
+    </>
+  )
 };
 
 export default App;
