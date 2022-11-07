8c6f16e6d62fa55720c8fbd6bf7badec965d24d6
DevFrog92 <yongsu1481@gmail.com>
2022-11-07 15:19:01 +0900

    ♻️ Refactoring useRef to useState

--- 06/star-rating/src/components/AddColorForm.js
+++ 06/star-rating/src/components/AddColorForm.js
@@ -1,25 +1,19 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 
 const AddColorForm = ({ onNewColor = f => f }) => {
-  const textTitle = useRef();
-  const hexColor = useRef();
+  const [title, setTitle] = useState('');
+  const [color, setColor] = useState('#000000');
 
   const submit = e => {
     e.preventDefault();
-
-    const title = textTitle.current.value;
-    const color = hexColor.current.value;
-
-    onNewColor(title, color);
-
-    textTitle.current.value = '';
-    hexColor.current.value = '#000000';
+    setTitle('');
+    setColor('#000000');
   }
 
   return (
     <form onSubmit={submit}>
-      <input ref={textTitle} type='text' placeholder='color title...' required />
-      <input ref={hexColor} type='color' required />
+      <input value={title} onChange={(e) => setTitle(e.target.value)} type='text' placeholder='color title...' required />
+      <input value={color} onChange={(e) => setColor(e.target.value)} type='color' required />
       <button>ADD</button>
     </form>
   )
