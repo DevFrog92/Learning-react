404eda864ab36ad37e4692957fd13ea2161d4d29
DevFrog92 <yongsu1481@gmail.com>
2022-11-15 18:16:17 +0900

    ✨ Implement Render Props example

--- 08/react-fetch-data/src/App.js
+++ 08/react-fetch-data/src/App.js
@@ -18,9 +18,36 @@ const GithubUser = ({ login }) => {
   return null;
 }
 
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10091},
+  { name: "Monument Peak", elevation: 10067},
+  { name: "Pyramid Peak", elevation: 9983},
+  { name: "Mt. Tallac", elevation: 9735},
+]
+
+function List({ data = [], renderEmpty, renderItem }) {
+  return !data.length ? (
+    renderEmpty) : (
+      <ul>
+        {
+          data.map((item, i) => (
+            <li key={i}>{ renderItem(item) }</li>
+          ))
+        }
+      </ul>
+    )
+}
 
 const App = () => {
-  return <GithubUser login="DevFrog92"/>
+  return <List
+    data={tahoe_peaks}
+    renderEmpty={<p>This List is empty</p>}
+    renderItem={item =>
+      <>
+        {item.name} - {item.elevation.toLocaleString()}ft
+      </>
+    }
+  />
 }
 
 export default App;
