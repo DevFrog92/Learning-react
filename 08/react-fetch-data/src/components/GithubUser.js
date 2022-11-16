e92d5e116792fb4ec860a0ba7f8dbe327069d3f2
DevFrog92 <yongsu1481@gmail.com>
2022-11-16 22:28:54 +0900

    🚚 Separate GithubUser component from App.js

---
+++ 08/react-fetch-data/src/components/GithubUser.js
@@ -0,0 +1,24 @@
+import Fetch from "./Fetch";
+
+const GithubUser = ({ login }) => {
+  return <Fetch uri={`https://api.github.com/users/${login}`} renderSuccess={ UserDetail} />
+}
+
+const UserDetail = ({ data }) => {
+  return (
+    <div className="githubUser">
+      <img
+        src={data.avatar_url}
+        alt={data.login}
+        style={{width: 200}}
+      />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{ data.name}</p>}
+        {data.location && <p>{ data.location}</p>}
+      </div>
+    </div>
+  );
+}
+
+export default GithubUser;
