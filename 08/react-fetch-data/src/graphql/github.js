e2ddda3a3ab6b1f23af51e118502f4a1e8ea6c63
DevFrog92 <yongsu1481@gmail.com>
2022-11-21 09:40:34 +0900

    🔥 Remove personal access github token

--- 08/react-fetch-data/src/graphql/github.js
+++ 08/react-fetch-data/src/graphql/github.js
@@ -5,7 +5,7 @@ export const client = new GraphQLClient(
   'https://api.github.com/graphql',
   {
     headers: {
-      Authorization: `Bearer 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`
+      Authorization: `Bearer <token>`
     }
   }
 );
